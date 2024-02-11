adf560070aa768a90356633ac829536b06a3588b
Malalo87 <paolamalagon23@gmail.com>
2024-02-11 18:18:23 -0500

    Primera Parte Jueguito

---
+++ Brasil.js
@@ -0,0 +1,5 @@
+alert('Amor! Amor! Amor! Amor! Bienvenido al concurso: Ganate un besito en el pipii - ππ!!');
+alert ('Adivina el número oculto!')
+
+let intento = prompt('Elige un numero entre 10 y 20');
+console.log(typeof('Valor de intento:', intento));
---
+++ ejercicio edad.js
@@ -0,0 +1,11 @@
+alert ('¡Bienvenida y bievenido a nuestro sitio web!');
+let msjError ='¡Error! Completa todos los campos';
+alert (msjError);
+let nombre = 'Luna';
+let edad = prompt(alert('Digita tu edad en años? '));
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+
+if (edad >= 18 ){
+    alert ('¡Puedes obtener tu licencia de conducir!')
+}
